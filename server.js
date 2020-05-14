a5548b18d57536d1329efedee4ad67c77cca96cd
alexandreNascimentoIFPE <alexandre.nascimentoti@gmail.com>
2020-05-14 16:18:27 -0300

    esqueleto

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app     = express()
+const http    = require('http').Server(app)
+const io      = require('socket.io')(http)
+const PORT    = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('conectado na porta :' + PORT)
+})
