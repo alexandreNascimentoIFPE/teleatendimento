485c7db014cbe42801e7b9f19ddae0bfb6d8f58e
alexandreNascimentoIFPE <alexandre.nascimentoti@gmail.com>
2020-05-18 13:25:30 -0300

    comit

--- server.js
+++ server.js
@@ -1,18 +1,31 @@
-const express = require('express')
-const app     = express()
-const http    = require('http').Server(app)
-const io      = require('socket.io')(http)
-const PORT    = process.env.PORT || 3000
-app.listen(PORT, () => {
-    console.log('conectado na porta :' + PORT)
-})
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
+// port setup and listening 
+http.listen(PORT, () => {
+    console.log("listening on port " + PORT)
+});
+
+// serving our index.html 
+app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
 })
-
+// allows files in this folder to be served all the time
 app.use(express.static('public'));
 
-io.on('connection', function(socket){
-    console.log("cliente esta conectado" + socket.id)
+// init socket 
+io.on('connection', function (socket) {
+    console.log("client is connected " + socket.id)
+    // receive the event, then send data to clients
+    socket.on('userMessage', (data) => {
+        io.sockets.emit("userMessage", data)
+    })
+    // receive the typing event, send out to clients
+    socket.on('userTyping', (data) => {
+        socket.broadcast.emit('userTyping', data)
+
+    });
 })
